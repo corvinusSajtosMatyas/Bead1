1629f6cd5941eccd9bcb91657f92d10db3c51cd5
sajtosM <matthias.sajtos@concircle.com>
2021-02-16 08:46:05 +0100

    🎉Initial commit

---
+++ src/Bead1/Bead1.java
@@ -0,0 +1,13 @@
+package Bead1;
+
+import java.util.Iterator;
+
+public class Bead1 {
+	public static void main(String[] args) {
+		String x = "123abc";
+
+		for (int i = 0; i < x.length(); i++) {
+			System.out.println(String.valueOf(x.charAt(i)));
+		}
+	}
+}
