360d2171ac3b7c667c7c415acc991e2a8fd5e318
sajtosM <matthias.sajtos@concircle.com>
2021-02-23 10:49:15 +0100

    🔨Add long text test

--- src/Bead1/Bead1.java
+++ src/Bead1/Bead1.java
@@ -4,8 +4,16 @@ import Util.Shifter;
 
 public class Bead1 {
 	public static void main(String[] args) {
-		Shifter s1 = new Shifter(args[0]);
+		String input = "";
+		if (args.length < 1) {
+			input = "alma,ALMA,Készítsen egy egyszerű titkosítást lehetővé alkalmazást, amely egy szöveges állományban a karaktereket a rákövetkezőre cseréli ki. (Pl alma bmnb) \n"
+					+ "Az alkalmazástól elvárjuk, hogy tetszőleges hosszúságú szöveges állományokat képes legyen kezelni.";
+		} else {
+			input = args[0];
+		}
+		Shifter s1 = new Shifter(input);
 		String shifted = s1.shifted();
+		System.out.println(shifted);
 	}
 
 }
